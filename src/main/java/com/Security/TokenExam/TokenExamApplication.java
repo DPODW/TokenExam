486575efcff55499bc35683ffb8ca4b9c41671a4
TaeJin Moon <tj001024@naver.com>
2023-07-20 02:47:29 +0900

    first commit

---
+++ src/main/java/com/Security/TokenExam/TokenExamApplication.java
@@ -0,0 +1,13 @@
+package com.Security.TokenExam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokenExamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Security/TokenExam/TokenExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Security.TokenExam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokenExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
